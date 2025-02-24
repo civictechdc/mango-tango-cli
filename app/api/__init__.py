26d23056b687a5225b6d490ced9595f03cd5498b
Stefan Netterfield <stefan@5f03.com>
2025-02-24 02:07:26 -0500

    Moved loop that creates the json data from the web presenters.

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -9,16 +9,20 @@ class APIContext(BaseModel):
     presenters_context: list[WebPresenterContext]
 
     def create_blueprint(self) -> Blueprint:
+        presenters = []
         output = Blueprint(
             "api",
             __name__,
             url_prefix="/api",
         )
 
+        for presenter_context in self.presenters_context:
+            presenters.append(presenter_context.web_presenter.api_factory(presenter_context))
+
         for endpoint in endpoints:
             output.add_url_rule(endpoint.path, view_func=endpoint.view.as_view(
                 endpoint.name,
-                self.presenters_context
+                presenters
             ))
 
         return output
--- app/api/routes/presenters.py
+++ app/api/routes/presenters.py
@@ -3,11 +3,8 @@ from flask.views import MethodView
 from analyzer_interface.context import WebPresenterContext
 
 class PresentersView(MethodView):
-    def __init__(self, presenters_context: list[WebPresenterContext]):
-        self.presenters = []
-
-        for presenter_context in presenters_context:
-            self.presenters.append(presenter_context.web_presenter.api_factory(presenter_context))
+    def __init__(self, presenters: list):
+        self.presenters = presenters
 
     def get(self):
         return jsonify({"code": 200, "data": self.presenters}), 200
