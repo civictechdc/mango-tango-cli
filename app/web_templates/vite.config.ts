b419d0f3ebf9b3b043b5d9e2f536788aa7ed3971
VEDA95 <snetterfield1@gmail.com>
2025-03-01 18:34:03 -0500

    WIP. Bulding out client-side router with @tanstack/react-router

--- app/web_templates/vite.config.ts
+++ app/web_templates/vite.config.ts
@@ -2,12 +2,17 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import TanStackRouterVite from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
     root: resolve(__dirname, './src'),
     publicDir: '../web_static',
     base: '/static',
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        TanStackRouterVite({target: 'react', autoCodeSplitting: true}),
+        tailwindcss()
+    ],
     build: {
         outDir: resolve(__dirname, './build'),
         manifest: 'manifest.json',
