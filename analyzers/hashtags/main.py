3212db43347b9b47a512d0120428147cfd1c7ab6
Kristijan Armeni <kristijan.armeni@gmail.com>
2025-03-08 21:25:31 -0500

    [ENH] extract hashtags directly from text and refactor

--- analyzers/hashtags/main.py
+++ analyzers/hashtags/main.py
@@ -42,36 +42,45 @@ def gini(x):
     return (n + 1 - 2 * sum(cumx) / cumx[-1]) / n
 
 
-def main(context: PrimaryAnalyzerContext):
+def hashtag_analysis(data_frame:pl.DataFrame, every="1h") -> pl.DataFrame:
 
-    input_reader = context.input()
-    df_input = input_reader.preprocess(pl.read_parquet(input_reader.parquet_path))
+    has_hashtag_symbol = pl.col(COL_HASHTAGS).str.contains("#").any()
+    extract_hashtags = pl.col(COL_HASHTAGS).str.extract_all(r'(#\S+)')
+    extract_hashtags_by_split = (
+        pl.col(COL_HASHTAGS)
+        .str.strip_chars("[]")
+        .str.replace_all("'", "")
+        .str.replace_all(" ", "")
+        .str.split(",")
+        )
 
     # assign None to messages with no hashtags
-    df_input = df_input.with_columns(
-        pl.when(pl.col(COL_HASHTAGS) == NULL_CHAR)
-        .then(None)
-        .otherwise(
-            pl.col(COL_HASHTAGS)
-            .str.strip_chars("[]")
-            .str.replace_all("'", "")
-            .str.replace_all(" ", "")
-            .str.split(",")
-        )  # split hashtags into List[str]
-        .name.keep()
-    )
+    if data_frame.select(has_hashtag_symbol).item():
+        df_input = (
+            data_frame
+            .with_columns(extract_hashtags)
+            .filter(pl.col(COL_HASHTAGS) != [])
+        )
+    else:
+        df_input = (
+            data_frame
+            .filter(pl.col(COL_HASHTAGS) == '[]')
+            .with_columns(extract_hashtags_by_split)
+        )
 
     # select columns
     df_input = df_input.select(pl.col(COLS_ALL))
 
+    breakpoint()
+
     df_agg = (
-        df_input.filter(pl.col(COL_HASHTAGS).is_not_null())
+        df_input.drop_nulls(pl.col(COL_HASHTAGS))
         .select(
             pl.col(COL_TIME),
             pl.col(COL_HASHTAGS),
         )
         .sort(COL_TIME)
-        .group_by_dynamic(COL_TIME, every="1h")  # this could be a parameter
+        .group_by_dynamic(COL_TIME, every=every)  # this could be a parameter
         .agg(
             pl.col(COL_HASHTAGS).explode().alias(OUTPUT_COL_HASHTAGS),
             pl.col(COL_HASHTAGS).explode().count().alias(OUTPUT_COL_COUNT),
@@ -86,7 +95,15 @@ def main(context: PrimaryAnalyzerContext):
         )
     )
 
-    print("Output preview:")
-    print(df_agg.head())
+    return df_agg
+
+def main(context: PrimaryAnalyzerContext):
+
+    input_reader = context.input()
+    df_input = input_reader.preprocess(pl.read_parquet(input_reader.parquet_path))
+
+    df_agg = hashtag_analysis(
+        data_frame=df_input,
+    )
 
     df_agg.write_parquet(context.output(OUTPUT_GINI).parquet_path)
