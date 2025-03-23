e1204f7ded50255769f146c761ddee5b833f0607
VEDA95 <snetterfield1@gmail.com>
2025-03-23 14:35:27 -0400

    Resolved type errors caused by weird type imports from echarts

--- app/web_templates/src/components/charts/scatter.tsx
+++ app/web_templates/src/components/charts/scatter.tsx
@@ -1,7 +1,9 @@
 import useChart from '@/lib/hooks/chart.ts';
 import type { ReactElement, FC } from 'react';
 import type { Dimensions } from '@/lib/hooks/chart.ts';
-import type { TopLevelFormatterParams, DatasetOption, XAXisOption, YAxisOption, SeriesOption$1 } from 'echarts';
+import type { TopLevelFormatterParams, DatasetOption } from 'echarts/types/dist/shared';
+import type { SeriesOption } from 'echarts';
+import type {XAXisComponentOption, YAXisComponentOption} from 'echarts/types/dist/echarts'
 
 interface ScatterPlotProps {
     data: DatasetOption | Array<DatasetOption>;
@@ -9,20 +11,20 @@ interface ScatterPlotProps {
         x: string;
         y: string;
     };
-    tooltipFormatter: (params: TopLevelFormatterParams) => string;
+    tooltipFormatter?: (params: TopLevelFormatterParams) => string;
 }
 
 export default function ScatterPlot({ data, labels, tooltipFormatter }: ScatterPlotProps): ReactElement<FC> {
     const dimensions: Dimensions = {width: 800, height: 600};
-    const xAxis: XAXisOption = {
+    const xAxis: XAXisComponentOption = {
         type: 'log',
         name: labels != null && labels.x.length > 0 ? labels.x : 'X',
     };
-    const yAxis: YAxisOption = {
+    const yAxis: YAXisComponentOption = {
         type: 'log',
         name: labels != null && labels.y.length > 0 ? labels.y : 'Y',
     };
-    const series: SeriesOption$1 = [
+    const series: Array<SeriesOption> = [
         {
             type: 'scatter',
             encode: {
--- app/web_templates/src/components/ngram_scatter.tsx
+++ app/web_templates/src/components/ngram_scatter.tsx
@@ -3,7 +3,7 @@ import ScatterPlot from '@/components/charts/scatter.tsx';
 import type { ReactElement, FC } from 'react';
 import type { ChartProps } from '@/components/charts/props.ts';
 import type { PresenterAxisData } from '@/lib/data/presenters.ts';
-import type {TopLevelFormatterParams, DatasetOption} from 'echarts';
+import type {TopLevelFormatterParams, DatasetOption} from 'echarts/types/dist/shared';
 
 export type NgramScatterPlotDataPoint = {
     ngram: string;
@@ -11,39 +11,44 @@ export type NgramScatterPlotDataPoint = {
     y: number;
 };
 
-export type NgramScatterPlotDataPoints = Array<NgramScatterPlotDataPoint>;
-
 export default function NgramScatterPlot({ presenter }: ChartProps): ReactElement<FC> {
-    const data: DatasetOption = useMemo(() => {
-        if (presenter == null) return [];
+    const data= useMemo<DatasetOption>((): DatasetOption => {
+        let dataset: DatasetOption = {dimensions: ['ngram', 'x', 'y']};
+
+        if (presenter == null) return dataset;
 
         const rawNgrams = presenter.ngrams as Array<string>;
         const rawX = presenter.x as Array<number>;
         const rawY = (presenter.y as PresenterAxisData)['total_repetition'] as Array<number>;
 
-        return {
-            dimensions: ['ngram', 'x', 'y'],
-            source: Array.from({length: rawX.length}, (_, index: number): NgramScatterPlotDataPoint => ({
-                ngram: rawNgrams[index],
-                x: rawX[index],
-                y: rawY[index],
-            }))
-        };
+        dataset.source = Array.from({length: rawX.length}, (_, index: number): NgramScatterPlotDataPoint => ({
+            ngram: rawNgrams[index],
+            x: rawX[index],
+            y: rawY[index],
+        }));
+
+        return dataset;
     }, [presenter]);
     const labels = {x: 'User Repetition', y: 'Total Repetition'};
     const tooltipFormatter = (params: TopLevelFormatterParams): string => {
+        const param: TopLevelFormatterParams = Array.isArray(params) ? params[0] : params;
+
+        if(!param.data) return '';
+
+        const data = param.data as NgramScatterPlotDataPoint;
+
         return `
             <div class="grid gap-1.5">
                 <div class="[&>svg]:text-zinc-500 flex w-full flex-wrap items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5 dark:[&>svg]:text-zinc-400">
-                    <span class="font-bold">${params.data.ngram}</span>
+                    <span class="font-bold">${data.ngram}</span>
                 </div>
                 <div class="[&>svg]:text-zinc-500 flex w-full flex-wrap items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5 dark:[&>svg]:text-zinc-400">
                     ${labels.x != null && labels.x.length > 0 ? `<span class="font-bold">${labels.x}:</span>` : ''}
-                    <span>${params.data.x}</span>
+                    <span>${data.x}</span>
                 </div>
                 <div class="[&>svg]:text-zinc-500 flex w-full flex-wrap items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5 dark:[&>svg]:text-zinc-400">
                     ${labels.y != null && labels.y.length > 0 ? `<span class="font-bold">${labels.y}:</span>` : ''}
-                    <span>${params.data.y}</span>
+                    <span>${data.y}</span>
                 </div>
             </div>
         `;
