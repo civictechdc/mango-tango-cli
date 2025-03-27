bf120d2e0493a9d36034770a851f0436bbcfaf5e
VEDA95 <snetterfield1@gmail.com>
2025-03-27 00:42:48 -0400

    Cleaned up dashboard layout and style. Along with adding toggle for dark mode

--- app/web_templates/src/components/presenter.tsx
+++ app/web_templates/src/components/presenter.tsx
@@ -2,6 +2,7 @@ import usePresentersState from '@/lib/state/presenters';
 import HistogramChart from '@/components/example_histogram.tsx';
 import NgramScatterPlot from '@/components/ngram_scatter.tsx';
 import TimeIntervalChart from '@/components/time_interval_bar_plot.tsx';
+import { Card, CardContent } from '@/components/ui/card.tsx';
 import type { ReactElement, FC } from 'react';
 import type { Presenter, PresenterCollection } from '@/lib/data/presenters';
 import type { GlobalPresentersState } from '@/lib/state/presenters';
@@ -12,10 +13,16 @@ export function PresenterView(): ReactElement<FC> {
     if (presenters.length === 0) return <p>No charts to show yet!</p>;
 
     const presenter: Presenter = presenters[0];
+    let component: ReactElement<FC> | null = null;
 
-    if (presenter.figure_type === 'histogram') return <HistogramChart presenter={presenter} />;
-    if (presenter.figure_type === 'scatter') return <NgramScatterPlot presenter={presenter} />;
-    if(presenter.figure_type === 'bar') return <TimeIntervalChart presenter={presenter} />;
+    if (presenter.figure_type === 'histogram') component = <HistogramChart presenter={presenter} />;
+    if (presenter.figure_type === 'scatter') component = <NgramScatterPlot presenter={presenter} />;
+    if(presenter.figure_type === 'bar') component = <TimeIntervalChart presenter={presenter} />;
+    if(!component) component = <p>No component to show...</p>;
 
-    return <p>No component to show...</p>;
+    return (
+        <Card>
+            <CardContent>{component}</CardContent>
+        </Card>
+    );
 }
