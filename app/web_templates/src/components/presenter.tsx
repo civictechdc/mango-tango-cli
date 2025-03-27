0b52607300cf0ee450783f37fcf69ff63dc4b966
VEDA95 <snetterfield1@gmail.com>
2025-03-26 23:08:46 -0400

    Implemented bar plot for time interval analyzer.

--- app/web_templates/src/components/presenter.tsx
+++ app/web_templates/src/components/presenter.tsx
@@ -1,6 +1,7 @@
 import usePresentersState from '@/lib/state/presenters';
 import HistogramChart from '@/components/example_histogram.tsx';
 import NgramScatterPlot from '@/components/ngram_scatter.tsx';
+import TimeIntervalChart from '@/components/time_interval_bar_plot.tsx';
 import type { ReactElement, FC } from 'react';
 import type { Presenter, PresenterCollection } from '@/lib/data/presenters';
 import type { GlobalPresentersState } from '@/lib/state/presenters';
@@ -13,7 +14,8 @@ export function PresenterView(): ReactElement<FC> {
     const presenter: Presenter = presenters[0];
 
     if (presenter.figure_type === 'histogram') return <HistogramChart presenter={presenter} />;
-    if (presenter.figure_type === 'scatter') return <NgramScatterPlot presenter={presenter} />
+    if (presenter.figure_type === 'scatter') return <NgramScatterPlot presenter={presenter} />;
+    if(presenter.figure_type === 'bar') return <TimeIntervalChart presenter={presenter} />;
 
     return <p>No component to show...</p>;
 }
