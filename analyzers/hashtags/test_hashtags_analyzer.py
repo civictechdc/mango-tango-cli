ff8ca1a8bf2be8e54993f459c00a0fcc27c10f0e
Kristijan Armeni <kristijan.armeni@gmail.com>
2025-04-09 10:38:33 -0400

    [ENH] add tests for gini()

--- analyzers/hashtags/test_hashtags_analyzer.py
+++ analyzers/hashtags/test_hashtags_analyzer.py
@@ -1,11 +1,79 @@
 import os
 
+import numpy as np
+import polars as pl
+
 from preprocessing.series_semantic import datetime_string, identifier, text_catch_all
 from testing import CsvTestData, JsonTestData, test_primary_analyzer
 
 from .interface import COL_AUTHOR_ID, COL_POST, COL_TIME, OUTPUT_GINI, interface
-from .main import main
+from .main import gini, main
 from .test_data import test_data_dir
+
+HASHTAGS = [
+    "sunset",
+    "nature",
+    "food",
+    "travel",
+    "happy",
+    "friends",
+    "love",
+    "family",
+    "music",
+    "art",
+    "photography",
+    "fun",
+    "smile",
+    "weekend",
+    "coffee",
+    "book",
+    "reading",
+    "running",
+    "fitness",
+    "health",
+    "cat",
+    "dog",
+    "bird",
+    "fish",
+    "lizard",
+]
+
+
+def test_gini():
+    test_cases = [
+        # a single hashtags is detected
+        {
+            "data": ["same"] * 1000,
+            "expected": 0.0,
+            "description": "One single hashtag (no inequality possible)",
+        },
+        {
+            "data": HASHTAGS,
+            "expected": 0.0,
+            "description": "Perfect equality (all unique)",
+        },
+        {
+            "data": HASHTAGS * 5,
+            "expected": 0.0,
+            "description": "Perfect equality (all appear 5 times)",
+        },
+        {
+            "data": ["trending"] * 9999 + HASHTAGS,
+            "expected": 0.95,  # It goes towards 1
+            "description": "Extreme inequality (9999 vs 1 occurrences)",
+        },
+    ]
+
+    # caculcate gini and compare
+    for test_case in test_cases:
+        data_series = pl.Series(test_case["data"])
+
+        # Calculate actual Gini coefficient
+        actual = gini(data_series)
+
+        assert np.allclose(
+            [actual], [test_case["expected"]], rtol=1e-2, atol=1e-2
+        ), f"Failed test case: {test_case['description']}"
 
 
 # This example shows you how to test a primary analyzer.
