169e3b2fc1f252a9a53c408abe04a0574eeca9c0
eliwss0 <eliwss0@gmail.com>
2025-04-15 10:11:37 -0400

    Added json import support (#122)
    * Added json import support
    
    * Formatting

--- importing/__init__.py
+++ importing/__init__.py
@@ -1,5 +1,10 @@
 from .csv import CSVImporter
 from .excel import ExcelImporter
 from .importer import Importer, ImporterSession
+from .json import JSONImporter
 
-importers: list[Importer[ImporterSession]] = [CSVImporter(), ExcelImporter()]
+importers: list[Importer[ImporterSession]] = [
+    CSVImporter(),
+    JSONImporter(),
+    ExcelImporter(),
+]
