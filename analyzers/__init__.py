49326210215d093732eaabe00519ae0cb3aca69f
Kristijan Armeni <kristijan.armeni@gmail.com>
2025-07-22 16:36:14 -0400

    [FEAT] Add tests for the ngram analyzer and reorganize the folders (#168)
    * test: initial commit, add test_ngrams.py
    
    * test: initial commit add .csv and .parquet data for testing
    
    * test: add ParquetTestData class
    
    * feat: sort the output of n-gram statistics, change print feedback
    
    * test: add __init__.py
    
    * refactor: move ngram analyzers to a single ngrams folder
    
    * refactor: update import statements
    
    * refactor: move and rename base test
    
    * test: add parquet data for ngram_stats test
    
    * initial commit, __init__.py
    
    * chore: pl.count() deprecated use pl.len()

--- analyzers/__init__.py
+++ analyzers/__init__.py
@@ -5,9 +5,9 @@ from .example.example_report import example_report
 from .example.example_web import example_web
 from .hashtags import hashtags
 from .hashtags_web import hashtags_web
-from .ngram_stats import ngram_stats
-from .ngram_web import ngrams_web
-from .ngrams import ngrams
+from .ngrams.ngram_stats import ngram_stats
+from .ngrams.ngram_web import ngrams_web
+from .ngrams.ngrams_base import ngrams
 from .temporal import temporal
 from .temporal_barplot import temporal_barplot
 from .time_coordination import time_coordination
