693641969d29e1027d5b46d90f18d5b8b63701e0
Kristijan Armeni <kristijan.armeni@gmail.com>
2025-08-07 18:45:50 -0400

    feat: update ascii art welcome page, use rich.Panel

--- components/splash.py
+++ components/splash.py
@@ -1,18 +1,34 @@
+from rich import print
+from rich.panel import Panel
+
 from meta import get_version
 from terminal_tools import clear_terminal, wait_for_key
 
 
 def splash():
     clear_terminal()
-    print(_ascii_splash)
-    print("")
-    print(f"{get_version() or '<development version>'}")
+    print(_ASCII_LOGO)
+    print(_ASCII_TREE)
     print("")
     wait_for_key(True)
 
 
-_ascii_splash: str = """
-       -..*+:..-
+_VERSION = f"[dim]{get_version() or 'development version'}[/dim]"
+
+_ASCII_LOGO = Panel.fit(
+    """[orange1]
+  ██████╗ ██╗ ██████╗      ███╗   ███╗  █████╗  ███╗   ██╗  ██████╗   ██████╗      ████████╗ ██████╗  ███████╗ ███████╗
+ ██╔════╝ ██║ ██╔══██╗     ████╗ ████║ ██╔══██╗ ████╗  ██║ ██╔════╝  ██╔═══██╗     ╚══██╔══╝ ██╔══██╗ ██╔════╝ ██╔════╝
+ ██║      ██║ ██████╔╝     ██╔████╔██║ ███████║ ██╔██╗ ██║ ██║  ███╗ ██║   ██║        ██║    ██████╔╝ █████╗   █████╗
+ ██║      ██║ ██╔══██╗     ██║╚██╔╝██║ ██╔══██║ ██║╚██╗██║ ██║   ██║ ██║   ██║        ██║    ██╔══██╗ ██╔══╝   ██╔══╝
+ ╚██████╗ ██║ ██████╔╝     ██║ ╚═╝ ██║ ██║  ██║ ██║ ╚████║ ╚██████╔╝ ╚██████╔╝        ██║    ██║  ██║ ███████╗ ███████╗
+  ╚═════╝ ╚═╝ ╚═════╝      ╚═╝     ╚═╝ ╚═╝  ╚═╝ ╚═╝  ╚═══╝  ╚═════╝   ╚═════╝         ╚═╝    ╚═╝  ╚═╝ ╚══════╝ ╚══════╝[/orange1]""",
+    title="A Civic Tech DC Project",
+    subtitle=_VERSION,
+)
+
+_ASCII_TREE: str = """
+        -..*+:..-.
        -.=-+%@%##+-=.-
     = =:*%:...=:..=@*:+ =
   :: -:=#==#*=:::-=-...-:::
@@ -26,14 +42,17 @@ _ascii_splash: str = """
             @@#=
             @@%
             @@@
-
- C I B   M A N G O   T R E E
-
-   A Civic Tech DC Project
-
-       ╱ * * *  ╱ ╲
-       ╲ ===== ╱  ╱
 """
+
+_FOOTER: str = Panel.fit(
+    """
+  A Civic Tech DC Project
+[red]
+       ╱ * * *  ╱ ╲
+       ╲ ===== ╱  ╱[/red]
+"""
+)
+
 """
 I generated this using https://www.asciiart.eu/image-to-ascii
 """
