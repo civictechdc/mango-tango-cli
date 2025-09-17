3d1da139ed4a7d70f1bf0c203cc6c646c08beb70
Kristijan Armeni <kristijan.armeni@gmail.com>
2025-09-17 14:35:12 -0400

    chore: update temporal imports

--- analyzers/__init__.py
+++ analyzers/__init__.py
@@ -8,8 +8,8 @@ from .hashtags.hashtags_web import hashtags_web
 from .ngrams.ngram_stats import ngram_stats
 from .ngrams.ngram_web import ngrams_web
 from .ngrams.ngrams_base import ngrams
-from .temporal import temporal
-from .temporal_barplot import temporal_barplot
+from .temporal.temporal_base import temporal
+from .temporal.temporal_web import temporal_web
 from .time_coordination import time_coordination
 
 suite = AnalyzerSuite(
@@ -22,7 +22,7 @@ suite = AnalyzerSuite(
         ngrams_web,
         time_coordination,
         temporal,
-        temporal_barplot,
+        temporal_web,
         hashtags,
         hashtags_web,
     ]
