990bf075061defe91c53714f8e28f1d52687daf6
JMCulhane <62526923+JMCulhane@users.noreply.github.com>
2025-09-18 12:34:55 -0400

    Updated naming conventions in CLI (#205)
    * Updated naming conventions in CLI
    * Update tests

--- analyzers/ngrams/ngrams_base/interface.py
+++ analyzers/ngrams/ngrams_base/interface.py
@@ -47,7 +47,7 @@ the corpus of text, and whether certain authors use these sequences more often.
         columns=[
             InputColumn(
                 name=COL_AUTHOR_ID,
-                human_readable_name="Message Author ID",
+                human_readable_name="Unique User ID",
                 data_type="identifier",
                 description="The unique identifier of the author of the message",
                 name_hints=[
@@ -63,7 +63,7 @@ the corpus of text, and whether certain authors use these sequences more often.
             ),
             InputColumn(
                 name=COL_MESSAGE_ID,
-                human_readable_name="Unique Message ID",
+                human_readable_name="Unique Post Number",
                 data_type="identifier",
                 description="The unique identifier of the message",
                 name_hints=[
@@ -77,7 +77,7 @@ the corpus of text, and whether certain authors use these sequences more often.
             ),
             InputColumn(
                 name=COL_MESSAGE_TEXT,
-                human_readable_name="Message Text",
+                human_readable_name="Post Content",
                 data_type="text",
                 description="The text content of the message",
                 name_hints=[
@@ -92,7 +92,7 @@ the corpus of text, and whether certain authors use these sequences more often.
             ),
             InputColumn(
                 name=COL_MESSAGE_TIMESTAMP,
-                human_readable_name="Message Timestamp",
+                human_readable_name="Timestamp",
                 data_type="datetime",
                 description="The time at which the message was posted",
                 name_hints=["time", "timestamp", "date", "ts"],
